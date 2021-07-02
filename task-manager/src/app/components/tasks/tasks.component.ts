2f9403facb19cc81231e2c9e881b5e9a5b89f634
Jordan Masone <jordanmasone1@gmail.com>
2021-07-02 16:03:54 -0400

    Added delete and update services for the tasks.

--- task-manager/src/app/components/tasks/tasks.component.ts
+++ task-manager/src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
 import { Task } from "../../Task";
-import { TASKS } from "../../mock-task";
 
 @Component({
   selector: 'app-tasks',
@@ -8,11 +8,28 @@ import { TASKS } from "../../mock-task";
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
+  // On load, we will call the task service function that gets the tasks array and then sets it to the empty task array
   ngOnInit(): void {
-  }
+    // Subscribe is similar to a promise method
+    this.taskService
+      .getTasks()
+      .subscribe((tasks) => this.tasks = tasks);
+  };
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter(t => t.id !== task.id)));
+  };
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  };
 
 }
