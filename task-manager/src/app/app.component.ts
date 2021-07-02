e097e263c14215a41aef343992150a7bd933a28e
Jordan Masone <jordanmasone1@gmail.com>
2021-07-02 14:30:23 -0400

    Initial file creation.

---
+++ task-manager/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+// Component Declaration
+// The selector is the actual HTML tag you will use to embed your component.
+// The template URL is the HTML file you are using with the component.
+// The style URL is the css file your are using with the component, you can have multiple css files.
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+// This class is where you add properties and methods
+export class AppComponent { }
